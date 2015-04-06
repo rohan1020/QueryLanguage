8dce6c272d7a18962c35cee3e34c9f7336b028b3
Rohan Raja <rohanraja9@gmail.com>
2015-04-06 22:47:54 +0530

    Initial Commit

---
+++ QueryLanguage/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QueryLanguage
+//
+//  Created by Rohan Raja on 06/04/15.
+//  Copyright (c) 2015 Rohan Raja. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
